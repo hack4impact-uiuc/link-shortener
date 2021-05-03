168363055580b43e82c3bdf8da0b1eb649563c85
Arpan Laha <arpanlaha99@gmail.com>
2021-05-03 18:50:26 +0000

    add recruitment redirects

--- api/handler.go
+++ api/handler.go
@@ -5,7 +5,10 @@ import (
 )
 
 var redirects = map[string]string{
-	"website": "https://uiuc.hack4impact.org",
+	"fa21-interest": "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform",
+	"fa21-join":     "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa",
+	"fa21-external": "https://docs.google.com/forms/d/e/1FAIpQLSeb-5WluYBsjBeCI7AIFoWHhLbmvRibn9eGkiAZ4obFsHHzUw/viewform",
+	"fa21-tech":     "https://docs.google.com/forms/d/e/1FAIpQLSeWADqb8Ge9jtQh-KSUax9MfBpZXepTSdwUijFMaocRhb3Sbw/viewform",
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {

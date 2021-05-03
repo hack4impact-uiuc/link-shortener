5ea7aba70f39e8b6ab783835accb889dd7172e2c
Arpan Laha <arpanlaha99@gmail.com>
2021-05-03 07:40:05 +0000

    serve index

--- api/index.go
+++ api/index.go
@@ -14,7 +14,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 
 	if path == "/" {
-		fmt.Fprintf(w, "index")
+		http.ServeFile(w, r, "./static/index.html")
 	} else if redirect, ok := redirects[path[1:]]; ok {
 		fmt.Fprint(w, redirect)
 	} else {
@@ -24,7 +24,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Fprintf(os.Stdout, "Web Server started. Listening on 0.0.0.0:80\n")
+	fmt.Fprintf(os.Stdout, "Web Server started. Listening on 0.0.0.0:8080\n")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.HandleFunc("/", Handler)
 	http.ListenAndServe(":8080", nil)
 }

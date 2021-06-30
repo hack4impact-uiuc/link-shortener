2a1a2f09f650507eb203765797c7452e8d259317
Arpan Laha <arpanlaha99@gmail.com>
2021-06-30 21:00:46 +0000

    add named links

--- api/handler.go
+++ api/handler.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 )
 
-var redirects = map[string]string{
-	"ly":          "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa",
-	"ly-interest": "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform",
-	// "ly-join":     "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa", // same as h4i.app/ly
+type Redirect struct {
+	link string
+	name string
+}
+
+var redirects = map[string]Redirect{
+	"ly":          {name: "Join us", link: "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa"},
+	"ly-interest": {name: "Interest form", link: "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform"},
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +22,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(redirects)
 	} else if redirect, ok := redirects[path[1:]]; ok {
-		http.Redirect(w, r, redirect, http.StatusMovedPermanently)
+		http.Redirect(w, r, redirect.link, http.StatusMovedPermanently)
 	} else {
 		http.NotFound(w, r)
 	}

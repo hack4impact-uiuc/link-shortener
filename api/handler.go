70aaf6fbdbe4b8495aae19c4ab03d8f1ee3ec8af
Arpan Laha <arpanlaha99@gmail.com>
2021-06-30 21:06:39 +0000

    export Redirect fields and add struct tags for json renaming

--- api/handler.go
+++ api/handler.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Redirect struct {
-	link string
-	name string
+	Name string `json:"name"`
+	Link string `json:"link"`
 }
 
 var redirects = map[string]Redirect{
-	"ly":          {name: "Join us", link: "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa"},
-	"ly-interest": {name: "Interest form", link: "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform"},
+	"ly":          {Name: "Join us", Link: "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa"},
+	"ly-interest": {Name: "Interest form", Link: "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform"},
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -22,7 +22,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(redirects)
 	} else if redirect, ok := redirects[path[1:]]; ok {
-		http.Redirect(w, r, redirect.link, http.StatusMovedPermanently)
+		http.Redirect(w, r, redirect.Link, http.StatusMovedPermanently)
 	} else {
 		http.NotFound(w, r)
 	}

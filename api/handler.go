35e2e82c3898e4c228c9a6bc76cf219434fce3e9
Arpan Laha <arpanlaha99@gmail.com>
2021-07-02 01:36:05 +0000

    add order field

--- api/handler.go
+++ api/handler.go
@@ -6,19 +6,20 @@ import (
 )
 
 type Redirect struct {
-	Name string `json:"name"`
-	Link string `json:"link"`
+	Name  string `json:"name"`
+	Link  string `json:"link"`
+	Order uint   `json:"order"`
 }
 
 var redirects = map[string]Redirect{
-	"website":     {Name: "Our Website", Link: "https://uiuc.hack4impact.org/"},
-	"ly":          {Name: "Join Us", Link: "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa"},
-	"ly-interest": {Name: "Interest Form", Link: "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform"},
-	"instagram":   {Name: "Instagram", Link: "https://www.instagram.com/hack4impactuiuc/"},
-	"facebook":    {Name: "Facebook", Link: "https://www.facebook.com/h4iuiuc"},
-	"github":      {Name: "GitHub", Link: "https://github.com/hack4impact-uiuc"},
-	"medium":      {Name: "Medium", Link: "https://medium.com/hack4impact-uiuc"},
-	"youtube":     {Name: "YouTube", Link: " https://www.youtube.com/channel/UCmnm4j_IYDP5YCJ-msOZBwg"},
+	"website":     {Name: "Our Website", Link: "https://uiuc.hack4impact.org/", Order: 0},
+	"ly":          {Name: "Join Us", Link: "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa", Order: 1},
+	"ly-interest": {Name: "Interest Form", Link: "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform", Order: 2},
+	"instagram":   {Name: "Instagram", Link: "https://www.instagram.com/hack4impactuiuc/", Order: 3},
+	"facebook":    {Name: "Facebook", Link: "https://www.facebook.com/h4iuiuc", Order: 4},
+	"github":      {Name: "GitHub", Link: "https://github.com/hack4impact-uiuc", Order: 5},
+	"medium":      {Name: "Medium", Link: "https://medium.com/hack4impact-uiuc", Order: 6},
+	"youtube":     {Name: "YouTube", Link: " https://www.youtube.com/channel/UCmnm4j_IYDP5YCJ-msOZBwg", Order: 7},
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {

9e076b675f306f60858aaf030e679b39fa5e9d3d
Shresta Bangaru <51917387+shresta4@users.noreply.github.com>
2021-08-18 16:55:19 -0700

    New interest form for fa21 recruitment updates (#4)
    * New interest form for fa21 recruitment updates
    
    * updating link
    
    * Adding info session link
    
    * Adding application link

--- api/handler.go
+++ api/handler.go
@@ -21,12 +21,14 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	redirectList := [...]RedirectListItem{
 		{Alias: "website", Name: "Our Website", Link: "https://uiuc.hack4impact.org/"},
 		{Alias: "ly", Name: "Student Application Info", Link: "https://www.notion.so/h4iuiuc/Join-Hack4Impact-UIUC-2e875ce396b34e6ebb23c6dda57a89aa"},
-		{Alias: "ly-interest", Name: "Student Interest Form", Link: "https://docs.google.com/forms/d/e/1FAIpQLSeRbPeglRvMHgLiTIh9mhWRe1NY2y4ki26BTTgsfrdY4UjqSw/viewform"},
+		{Alias: "ly-interest", Name: "Student Interest Form", Link: "https://forms.gle/UFgjwzU9GieocTmV9"},
 		{Alias: "instagram", Name: "Instagram", Link: "https://www.instagram.com/hack4impactuiuc/"},
 		{Alias: "facebook", Name: "Facebook", Link: "https://www.facebook.com/h4iuiuc"},
 		{Alias: "github", Name: "GitHub", Link: "https://github.com/hack4impact-uiuc"},
 		{Alias: "medium", Name: "Medium", Link: "https://medium.com/hack4impact-uiuc"},
 		{Alias: "youtube", Name: "YouTube", Link: " https://www.youtube.com/channel/UCmnm4j_IYDP5YCJ-msOZBwg"},
+		{Alias: "info-session", Name: "Info Session", Link: "https://www.facebook.com/events/1907479396095709/?ref=newsfeed"}, 
+		{Alias: "apply", Name: "Application", Link: "https://forms.gle/xh6kFu38PGFDcTZY9"}, 
 	}
 
 	redirects := make(map[string]Redirect)

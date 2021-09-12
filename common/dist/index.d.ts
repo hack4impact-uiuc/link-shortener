68dd01155daa0f61a01d9b40cf6ef70095619127
Arpan Laha <arpanlaha99@gmail.com>
2021-09-12 07:32:50 +0000

    remove dist

--- common/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export interface AliasedLinkType {
-    alias: string;
-    destination: string;
-    name: string;
-    public: boolean;
-}
-export declare const AliasedLink: mongoose.Model<AliasedLinkType, {}, {}>;
-export declare function mongoConnect(): Promise<any>;

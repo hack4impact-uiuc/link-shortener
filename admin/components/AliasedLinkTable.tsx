dbb911db44b80fa0b4df544e56059d5c4a0bb6f1
Arpan Laha <arpanlaha99@gmail.com>
2021-09-12 19:51:04 +0000

    add filters/sorting to table

--- admin/components/AliasedLinkTable.tsx
+++ admin/components/AliasedLinkTable.tsx
@@ -4,44 +4,81 @@ import DeleteButton from "./DeleteButton";
 import EditButton from "./EditButton";
 import { AliasedLinkType } from "../utils";
 
+const { Column } = Table;
+
 interface AliasedLinkTableProps {
   aliasedLinks: AliasedLinkType[];
+}
+
+function compareStrings(a: string, b: string): number {
+  if (a === b) {
+    return 0;
+  }
+
+  return a < b ? -1 : 1;
 }
 
 export default function AliasedLinkTable(props: AliasedLinkTableProps) {
   const { aliasedLinks } = props;
 
-  const columns: ColumnsType<AliasedLinkType> = [
-    { title: "Name", dataIndex: "name", key: "name" },
-    { title: "Alias", dataIndex: "alias", key: "alias" },
-    {
-      title: "Destination",
-      dataIndex: "destination",
-      key: "destination",
-      render: (destination) => (
-        <a href={destination} target="_blank" rel="noopener noreferrer">
-          {destination}
-        </a>
-      ),
-    },
-    {
-      title: "Visibility",
-      dataIndex: "public",
-      key: "public",
-      render: (isPublic) => (isPublic ? "Public" : "Private"),
-    },
-
-    {
-      title: "Actions",
-      key: "actions",
-      render: (_, aliasedLink) => (
-        <Space>
-          <EditButton aliasedLink={aliasedLink} />
-          <DeleteButton aliasedLink={aliasedLink} />
-        </Space>
-      ),
-    },
-  ];
-
-  return <Table dataSource={aliasedLinks} columns={columns} />;
+  return (
+    <Table
+      dataSource={aliasedLinks}
+      rowKey={(aliasedLink: AliasedLinkType) => aliasedLink.alias}
+      pagination={{ hideOnSinglePage: true }}
+    >
+      <Column
+        title="Name"
+        dataIndex="name"
+        key="name"
+        sorter={(a: AliasedLinkType, b: AliasedLinkType) =>
+          compareStrings(a.name, b.name)
+        }
+      />
+      <Column
+        title="Alias"
+        dataIndex="alias"
+        key="alias"
+        sorter={(a: AliasedLinkType, b: AliasedLinkType) =>
+          compareStrings(a.alias, b.alias)
+        }
+      />
+      <Column
+        title="Destination"
+        dataIndex="destination"
+        key="destination"
+        render={(destination) => (
+          <a href={destination} target="_blank" rel="noopener noreferrer">
+            {destination}
+          </a>
+        )}
+        sorter={(a: AliasedLinkType, b: AliasedLinkType) =>
+          compareStrings(a.destination, b.destination)
+        }
+      />
+      <Column
+        title="Visibility"
+        dataIndex="public"
+        key="public"
+        render={(isPublic) => (isPublic ? "Public" : "Private")}
+        filters={[
+          { text: "Public", value: true },
+          { text: "Private", value: false },
+        ]}
+        onFilter={(value, aliasedLink: AliasedLinkType) =>
+          aliasedLink.public === value
+        }
+      />
+      <Column
+        title="Actions"
+        key="actions"
+        render={(_, aliasedLink: AliasedLinkType) => (
+          <Space>
+            <EditButton aliasedLink={aliasedLink} />
+            <DeleteButton aliasedLink={aliasedLink} />
+          </Space>
+        )}
+      />
+    </Table>
+  );
 }

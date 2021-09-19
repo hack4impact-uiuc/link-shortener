9dcd0f202535e1a47f64915244ed919416ba620e
Arpan Laha <arpanlaha99@gmail.com>
2021-09-19 02:31:06 +0000

    add authwrap utils file

---
+++ admin/pages/api/links/_utils.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { mongoConnect } from "../../../utils";
+
+export async function authWrap(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  callback: (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+): Promise<void> {
+  const session = await getSession({ req });
+  switch (session?.user?.email) {
+    case process.env.ADMIN_EMAIL: {
+      await mongoConnect();
+      await callback(req, res);
+    }
+
+    case undefined: {
+      res.status(401);
+    }
+
+    default: {
+      res.status(403);
+    }
+  }
+}
